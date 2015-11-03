5746f4871a8d8f9963ff9015b7282b5eaf0b23b2
Rodion Smakota <rsmakota@gmail.com>
2015-11-03 23:12:18 +0200

    Manage

---
+++ src/Ortofit/Bundle/BackOfficeBundle/Controller/ManageController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @copyright 2015 ortofit_quiz
+ * @author Rodion Smakota <rsmakota@gmail.com>
+ */
+
+namespace Ortofit\Bundle\BackOfficeBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Class ManageController
+ *
+ * @package Ortofit\Bundle\BackOfficeBundle\Controller
+ */
+class ManageController extends Controller
+{
+
+    public function getAction()
+    {
+
+    }
+}
